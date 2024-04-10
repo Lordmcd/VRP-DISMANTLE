a042403ba1e9e4c95d1ae10d634e8af03c3c92c3
Lordmcd <luisguilhermemcd@gmail.com>
2024-04-10 14:11:36 -0300

    Solved

--- stack_desmanche/server.lua
+++ stack_desmanche/server.lua
@@ -6,4 +6,3 @@ RegisterNetEvent('STACK_DESMANCHE:PAYMENT', function()
     local user_id = vRP.getUserId(source)
     vRP.giveInventoryItem(user_id,'dinheiro-sujo',math.random(1000, 1500))
 end)
--- Teste
