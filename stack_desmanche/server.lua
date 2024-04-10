bbfb79620dfd4ccf69112034738cef37f3468774
Lordmcd <luisguilhermemcd@gmail.com>
2024-04-10 14:11:09 -0300

    Test

--- stack_desmanche/server.lua
+++ stack_desmanche/server.lua
@@ -5,5 +5,5 @@ local vRP = Proxy.getInterface('vRP')
 RegisterNetEvent('STACK_DESMANCHE:PAYMENT', function()
     local user_id = vRP.getUserId(source)
     vRP.giveInventoryItem(user_id,'dinheiro-sujo',math.random(1000, 1500))
-    --   lalala
 end)
+-- Teste
